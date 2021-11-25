b469e34081c9c0460c121a81289f9a4c8fcab4d0
DESKTOP-723MR3D\Mohd Maroof <kayamkhan2001@gmail.com>
2021-11-25 14:20:02 +0530

    Add Camera

--- lib/Pages/CameraPage.dart
+++ lib/Pages/CameraPage.dart
@@ -1,46 +1,93 @@
 // ignore_for_file: file_names, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-// import 'package:camera/camera.dart';
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-// late List<CameraDescription>? cameras;
+late List<CameraDescription> cameras;
 
 class CameraPage extends StatefulWidget {
-  const CameraPage({Key? key}) : super(key: key);
+  // const CameraPage({Key? key}) : super(key: key);
 
   @override
   _CameraPageState createState() => _CameraPageState();
 }
 
 class _CameraPageState extends State<CameraPage> {
-  // late CameraController controller;
+  late CameraController controller;
   // XFile? pictureFile;
-  // @override
-  // void initState() {
-  //   // TODO: implement initState
-  //   super.initState();
-  //   controller = CameraController(cameras![0], ResolutionPreset.max);
-  //   controller.initialize().then((_) {
-  //     if (!mounted) {
-  //       return;
-  //     }
-  //     setState(() {});
-  //   });
-  // }
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(cameras[0], ResolutionPreset.max);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
+  }
 
-  // @override
-  // void dispose() {
-  //   controller?.dispose();
-  //   super.dispose();
-  // }
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // if (!controller.value.isInitialized) {
-    //   return Container();
-    // }
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
     return Scaffold(
-        // body: CameraPreview(controller),
-        );
+        body: Stack(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: CameraPreview(controller),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 200,
+              color: Colors.transparent,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.flash_off,
+                        color: Colors.white,
+                        size: 30,
+                      )),
+                  SizedBox(
+                    height: 80,
+                    width: 80,
+                    child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.circle_sharp,
+                          color: Colors.red,
+                          size: 80,
+                        )),
+                  ),
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.cameraswitch_rounded,
+                        color: Colors.white,
+                        size: 30,
+                      )),
+                ],
+              ),
+            ),
+          ],
+        )
+      ],
+    ));
   }
 }
